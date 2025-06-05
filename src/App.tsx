4573c003a200cd8f563a66694e95598d9a8c68bd
Fredrik <3tomu24.andefr@folkuniversitetet.nu>
2025-06-05 09:47:42 +0200

    add methods to get price and info and show pricechart

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,47 @@
-import { useState } from 'react'
+import {useState} from 'react'
 import './App.css'
+import api from "./api/charging-station.tsx";
+import PriceChart from "./components/PriceChart.tsx";
+
+
+interface Info {
+    sim_time_hour: number;
+    sim_time_min: number;
+    base_current_load: number;
+    battery_capacity_kWh: number;
+    ev_battery_charge_start_stopp: number;
+}
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    const [info, setInfo] = useState<Info>();
+    const [price, setPrice] = useState<Array<number>>([]);
+
+    const handleGetCharge = () => {
+        api.get(`/info`)
+            .then((response) => {
+                setInfo(response.data);
+                console.log(response.data);
+            })
+    }
+
+    const handleGetPrice = () => {
+        setPrice([]);
+        api.get(`/priceperhour`)
+            .then((response) => {
+                setPrice(response.data);
+                console.log(response.data);
+            })
+    }
+
+    return (
+        <>
+            <button onClick={handleGetCharge}>Get Charge</button>
+            <p>{info ? info.battery_capacity_kWh : "No info"}</p>
+            <button onClick={handleGetPrice}>Get Price</button>
+            <PriceChart data={price}/>
+        </>
+    )
 }
 
 export default App
