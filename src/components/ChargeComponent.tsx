9d5ae5ac02d3b001edfb10e48a618ff7b2211d2a
Fredrik <3tomu24.andefr@folkuniversitetet.nu>
2025-06-11 10:03:37 +0200

    change to ReactApexCharts

--- src/components/ChargeComponent.tsx
+++ src/components/ChargeComponent.tsx
@@ -1,30 +1,41 @@
-import Chart from "react-apexcharts";
+import ReactApexChart from "react-apexcharts";
+import {useState} from "react";
+import type {ApexOptions} from "apexcharts";
 
-const ChargeComponent = (props: { data: number, charging: boolean })=> {
+const ChargeComponent = (props: { data: number, charging: boolean }) => {
     const charge = Number(props.data.toFixed(0));
     const series = [charge];
+/*    const [series, setSeries] = useState([charge]);*/
 
-    const options = {
+    const [options] = useState<ApexOptions>({
+        stroke: {
+            lineCap: 'round',
+
+        },
         plotOptions: {
             radialBar: {
                 endAngle: 135,
                 startAngle: -135,
                 hollow: {
                     margin: 0,
-                    size: '60%',
-                    background: "opaque",
-                    backgroundColor: "#24292e",
+                    size: '55%',
+                    background: '#091124',
                     dropShadow: {
                         enabled: true,
-                        top: -15,
-                        left: 0,
+                        top: 4,
+                        left: 4,
                         blur: 4,
                         opacity: 0.4
                     }
                 },
                 track: {
-                    background: "#f2f2f2",
-                    radius: 5,
+                    dropShadow: {
+                        enabled: true,
+                        top: 4,
+                        left: 4,
+                        blur: 4,
+                        opacity: 0.4
+                    }
                 },
                 dataLabels: {
                     name: {
@@ -32,23 +43,22 @@ const ChargeComponent = (props: { data: number, charging: boolean })=> {
                         offsetY: -20,
                         fontSize: '1.6em',
                         color: 'grey',
-
                     },
                     value: {
                         offsetY: 15,
                         fontSize: '2.4em',
-                        color: 'gray',
-                    }
-                }
-            }
+                        color: 'grey',
+                    },
+                },
+            },
         },
         labels: ['Charge'],
-    };
+    });
 
     return (
         <>
-            <div>
-                <Chart options={options} series={series} type="radialBar"/>
+            <div style={{marginBottom: "1.6em"}}>
+                <ReactApexChart options={options} series={series} type="radialBar" />
             </div>
 
         </>
